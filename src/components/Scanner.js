4cd63ad2646f39e5450b896606cefe892cc23902
Amanda Pouget <amanda.pouget@rolemodelsoftware.com>
2018-04-11 16:44:29 -0400

    Add green boxes to show QuaggaJS is searching for the barcode

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -53,6 +53,27 @@ class Scanner extends Component {
       Quagga.start();
     });
     Quagga.onDetected(this._onDetected.bind(this));
+    Quagga.onProcessed(function(result) {
+      const drawingCtx = Quagga.canvas.ctx.overlay;
+      const drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+          result.boxes.filter(box => box !== result.box).forEach(box => {
+            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+        }
+      }
+    });
   }
 
   componentWillUnmount() {
